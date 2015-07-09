bd44794ab1f1b2993de767d437e97fdb4cdbbf56
unknown <wilde.rossi@MGA879.db1.com.br>
2015-07-09 12:41:30 -0300

    Primeiro commit, enviando a base da aplicação

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var imageToAsciiConsole = require('ascii-images');
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/SampleApplication/Index/index.html');
+});
+
+var port = 3000;
+http.listen(port, function(){		
+	imageToAsciiConsole(__dirname + '/db1-logo.png', function(result){
+  		console.log(result);
+		console.log('Rodando em http://localhost:' + port);
+	});
+});
