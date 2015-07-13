d3fae5736e7d349ccc3fd5d52adc1440ae7ce643
unknown <wilde.rossi@MGA879.db1.com.br>
2015-07-13 14:47:40 -0300

    Mensagens de erro feitas, agora falta revisar o bug de mensagens iguais

--- CustomLibraries/Messages/Error/ErrorMessages.js
+++ CustomLibraries/Messages/Error/ErrorMessages.js
@@ -4,6 +4,22 @@ angular.module('messages').directive('errorMessages', function(){
 		restrict: 'E',
 		scope: {
 			errorArray: '='
+		},
+		link: function(scope, element, attrs, ctrl){
+			scope.visibleWindow = false;
+
+			scope.$watch('errorArray', function(newValue){
+				if (newValue.length > 0) {
+					scope.visibleWindow = true;
+				}
+			}, true);
+
+			scope.close = function(){
+				scope.visibleWindow = false;
+				setTimeout(function(){
+					scope.errorArray.splice(0, scope.errorArray.length);
+				}, 500);
+			};
 		}
 	};
 });
--- SampleApplication/JS/ErrorMessages.js
+++ SampleApplication/JS/ErrorMessages.js
@@ -1,3 +1,16 @@
 angular.module('sample-application').controller('sample-error', function($scope){
-	$scope.errors = ['asdfasdfasdasd'];
+	$scope.errors = [];
+	$scope.entity = {
+		message1: '', message2: ''
+	};
+	$scope.addErrorMessage = function(entity){
+		if (entity.message1) { 
+			$scope.errors.push(angular.copy(entity.message1));
+			delete entity.message1;
+		}
+		if (entity.message2) { 
+			$scope.errors.push(angular.copy(entity.message2));
+			delete entity.message2;
+		}
+	};
 });
